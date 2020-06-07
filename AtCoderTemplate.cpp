aae965381086400ce67dbe94a7e5dc03ce717708
Tai-Yaki <t.yamada@chapter-5.com>
2020-06-08 00:05:17 +0900

    add template

---
+++ AtCoderTemplate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <unordered_map>
+#include <cmath>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+int main()
+{
+
+}
