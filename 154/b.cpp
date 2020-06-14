1558124100a064fa0c8f1017c908655ec89732e5
Tai-Yaki <t.yamada@chapter-5.com>
2020-06-14 18:44:00 +0900

    abc154_b

---
+++ 154/b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <unordered_map>
+#include <cmath>
+#include <algorithm>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+int main()
+{
+  string s;
+  cin >> s;
+  for (int i = 0; i < s.length(); i++)
+  {
+    cout << "x";
+  }
+
+  return 0;
+}
