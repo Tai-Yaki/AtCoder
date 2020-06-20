9738c2f22c3442d7dd705e3e96ba9c7ec5301b5f
Tai-Yaki <t.yamada@chapter-5.com>
2020-06-20 23:50:30 +0900

    abc147_a

---
+++ 147/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <cmath>
+#include <algorithm>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+int main()
+{
+
+}
