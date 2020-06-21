41eadcdd39eb01e83a9c5d4dde7c5f2062677fdf
Tai-Yaki <t.yamada@chapter-5.com>
2020-06-21 22:42:17 +0900

    abc171_a

---
+++ 171/a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+int main()
+{
+  char a;
+  cin >> a;
+  if (a >= 'a' && a <= 'z') cout << "a";
+  else cout << "A";
+}
