d793d652768e3e817cb3443dddfaf69ea3113c69
Tai-Yaki <t.yamada@chapter-5.com>
2020-06-26 23:49:42 +0900

    abc140_a

---
+++ 140/a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  cout << pow(n, 3);
+}
